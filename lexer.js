94d50323b0382d4e22893e5b91fa66dd57934caf
Alexey <ava12@newmail.ru>
2015-08-15 11:14:01 +0300

    *

--- lexer.js
+++ lexer.js
@@ -1,111 +1,79 @@
+function BackLexerException(message, line, pos) {
+	this.line = line
+	this.pos = pos
+	this.message = message = ', line ' + line + ', char ' + pos
+}
 
-function ALexerException(Msg, Source, Pos) {
-	this.Message = Msg
-	Source = Source.substr(0, Pos).split('\n')
-	this.Line = Source.length
-	Source = Source.pop().replace(/^\r/, '')
-	this.Char = Source.length + 1
+function BackToken(type, data, line) {
+	this.type = type
+	this.data = data
+	this.line = line
+}
 
-	this.toString = function() {
-		return this.Message + ' @' + this.Line + ',' + this.Char
+BackToken.prototype.types = {
+	name: 'name', // data: имя
+	number: 'number', // data: число
+	string: 'string', // data: строка
+	label: 'label', // data: имя
+	brace: 'brace', // data: "(" или ")"
+	meta: 'meta' // data: команда
+}
+
+function BackLexer(source) {
+	this.source = source.replace(/\r\n?/g, '\n') + '\n'
+	this.sourceRe = /\n|\(|\)|\\::|\\.|"|'|(-?[0-9]+|0x[0-9a-fA-F]{1,4})|([^\0- \(\)\\"]+)/g
+	this.lineNumber = 1
+	this.linePos = 0
+	this.match = null
+}
+
+BackLexer.prototype.next = function () {
+	var value
+
+	while (true) {
+		this.match = this.sourseRe.exec(this.source)
+		if (!this.match) return null
+
+		if (this.match[1]) {
+			value = Number(this.match[1])
+			if (value < -32768 || value > 65535) {
+				throw this.newException('number out of range')
+			}
+
+			return this.newToken(BackToken.types.number, value)
+		}
+
+		if (this.match[2]) {
+			value = this.match[2]
+			if (value.charAt(0) != ':') return this.newToken(BackToken.types.name, value)
+			else return this.newToken(BackToken.types.label, value.substr(1))
+		}
+
+		value = this.match[0]
+		switch (value.charAt(0)) {
+			case '\n':
+				this.linePos = this.match.index + 1
+				this.lineNumber++
+			continue
+
+			case '(':
+			case ')':
+				return this.newToken(BackToken.types.brace, value)
+
+			case '"':
+			case "'":
+				return this.stringToken()
+
+			default:
+				return this.newToken(BackToken.types.meta, value)
+		}
 	}
 }
 
-function ALexer(Source) {
-	this.Source = Source
-	this.Re = new RegExp(
-		'\\(|\\)|!!|!|;' + // скобки и пр.
-		'|\\\\\\\\.*?(?:\\n|$)' + // однострочный комментарий
-		'|\\\\\\*.*?\\*\\\\' + // многострочный комментарий
-		'|"(?:""|[^"])*"' + // строка в двойных кавычках
-		"|'(?:''|[^'])*'" + // строка в одинарных кавычках
-		'|(0[xX][0-9A-Fa-f]+|-[0-9]+|[0-9]+)' + // число
-		'|([^\\s"\'()!;]+)' + // произвольный допустимый набор
-		'|(\\S)', // недопустимый набор
-		'g')
-	this.LastMatch = null
-	this.BraceStack = []
-	this.EoS = false
+BackLexer.prototype.newToken = function (type, data) {
+	return new BackToken(type, data, this.lineNumber)
+}
 
-//---------------------------------------------------------------------------
-	this.Emit = function(Type, Pos, Value) {
-		var Result = this.LastMatch
-		if (Type == undefined) this.LastMatch = null
-		else this.LastMatch = {Type: Type, Pos: Pos, Value: Value}
-		if (!this.LastMatch) this.EoS = true
-		return Result
-	}
-
-//---------------------------------------------------------------------------
-	this.Next = function() {
-		if (!this.Source || this.EoS) return null
-
-		var Result
-		while(true) {
-			var Match = this.Re.exec(this.Source)
-			if (!Match) {
-				if (!this.BraceStack.length) return this.Emit()
-				else throw new ALexerException('unexpected end of file', this.Source, this.Source.length)
-			}
-
-			if (Match[3]) {
-				throw new ALexerException('unexpected character: ' + Match[3], this.Source, Match.index)
-			}
-
-			if (Match[2]) {
-				if (Match[2] == '\\*') {
-					throw new ALexerException('unexpected end of file', this.Source, this.Source.length)
-				}
-
-				Result = this.Emit('', Match.index, Match[2])
-				if (Result) return Result
-				else continue
-			}
-
-			if (Match[1]) {
-				Result = parseInt(Match[1])
-				if (isNaN(Result)) {
-					throw new ALexerException('invalid number: ' + Match[1], this.Source, Match.index)
-				}
-
-				Result = this.Emit('1', Match.index, Result & 0xFFFF)
-				if (Result) return Result
-				else continue
-			}
-
-			switch(Match[0].substr(0, 1)) {
-				case '\\':
-					continue
-				break
-
-				case '(':
-					this.BraceStack.push(this.LastMatch)
-					this.LastMatch = null
-					continue
-				break
-
-				case ')':
-					if (!this.BraceStack.length) {
-						throw new ALexerException('unexpected )', this.Source, Match.index)
-					}
-
-					Result = this.LastMatch
-					this.LastMatch = this.BraceStack.pop()
-					if (Result) return Result
-				break
-
-				case '"': case "'":
-					Result = Match[0].substr(0, 1)
-					Result = Match[0].replace(new RegExp(Result + '(' + Result + '?)', 'g'), '$1')
-					Result = this.Emit('"', Match.index, Result)
-					if (Result) return Result
-				break
-
-				case '!': case ';':
-					Result = this.Emit('', Match.index, Match[0])
-					if (Result) return Result
-				break
-			}
-		}
-	}
+BackLexer.prototype.newException = function (message) {
+	return new BackLexerException(message, this.lineNumber, this.match.index - this.linePos + 1)
 }
---
+++ machine.js
@@ -0,0 +1,283 @@
+function BackMachine(input, output, program) {
+	this.status = this.statuses.manual
+	this.setInput(input)
+	this.setOutput(output)
+	this.memory = {}
+	this.program = this.setProgram(0, program)
+	this.ip = 0
+	this.callStack = []
+	this.operandStack = []
+	this.breakPoints = new Array(16)
+	for (var i = 0; i < 16; i++) this.breakPoints[i] = 0
+	this.breakFlags = 0
+	this.breakPointIndex = {}
+}
+
+BackMachine.prototype.opcodes = {
+	hlt: 0,
+	ret: 1,
+	call: 2,
+	jmp: 3,
+	ld: 4,
+	st: 5,
+	ins: 6,
+	outs: 7,
+	sh: 8,
+	sa: 9,
+	test: 10,
+	bt: 11,
+	dn: 12,
+	db: 13,
+	dw: 14,
+	dup: 15
+}
+
+BackMachine.prototype.statuses = {
+	manual: 0,
+	halt: 1,
+
+	breakPoint: 3,
+	input: 4,
+	output: 5,
+	operandStackEmpty: 6,
+	operandStackFull: 7,
+	callStackEmpty: 8,
+	callStackFull: 9
+}
+
+BackMachine.prototype.nullIo = {
+	read: function () {},
+	write: function () {}
+}
+
+BackMachine.prototype.getBreakPoint = function (index) {
+	return this.breakPoints[index & 15]
+}
+
+BackMachine.prototype.setBreakPoint = function (index, address) {
+	index &= 15
+	this.breakPoints[index] = address & 0xffff
+	this.breakPointIndex = {}
+	if (this.breakFlags) {
+		var flags = this.breakFlags
+		var mask = 1
+		for (index = 0; index < 16; index++) {
+			if (flags & mask) {
+				address = this.breakPoints[index]
+				if (this.breakPointIndex[address] == undefined) {
+					this.breakPointIndex[address] = index
+				}
+			}
+			mask <<= 1
+		}
+	}
+}
+
+BackMachine.prototype.toggleBreakPoint = function (index, on) {
+	index &= 15
+	var flags = this.breakFlags
+	var mask = 1 << index
+	if (on != undefined) {
+		var newFlags = (on ? flags | mask : flags & (~mask))
+		if (newFlags != flags) this.breakFlags = newFlags
+	}
+	return !!(flags & mask)
+}
+
+BackMachine.prototype.getMemory = function (address, length) {
+	var result
+	if (!length || lenght < 0) {
+		result = this.memory[address]
+		return (result ? result : 0)
+	}
+
+	result = new Array(length)
+	address &= 0xffff
+	for (var i = 0; i < length; i++) {
+		var word = this.memory[address]
+		result[i] = (word ? word : 0)
+		address = (address + 1) & 0xffff
+	}
+
+	return result
+}
+
+BackMachine.prototype.setMemory = function (address, values) {
+	address &= 0xffff
+	if (!(values instanceof Array)) values = [values]
+	for (var i = 0; i < values.length; i++) {
+		var value = Number(values[i])
+		if (value) this.memory[address] = value
+		else delete this.memory[address]
+		address = (address + 1) & 0xffff
+	}
+}
+
+BackMachine.prototype.getProgram = function (address, length) {
+	var result = this.program.substr(address, length)
+	if (result.length < length) result += (new Array(length - result.length + 1)).join('0')
+	return result
+}
+
+BackMachine.prototype.setProgram = function (address, program) {
+	address &= 0xffff
+	if (program instanceof Array) {
+		for (var i = 0; i < program.length; i++) {
+			var ch = program[i]
+			if (typeof ch == 'number') program[i] = String.fromCharCode(ch & 0xffff)
+		}
+		program = program.join('')
+	}
+
+	if ((program.length + address) > 0x10000) program = program.substr(0, 0x10000 - address)
+	if (this.program.length < address) {
+		this.program += (new Array(address - this.program.length + 1)).join('0')
+	}
+	this.program = this.program.substr(0, address) + program + this.program.substr(address + program.length)
+}
+
+BackMachine.prototype.setInput = function (input) {
+	this.input = (input ? input : this.nullIo)
+}
+
+BackMachine.prototype.setOutput = function (output) {
+	this.output = (output ? output : this.nullIo)
+}
+
+BackMachine.prototype.step = function () {
+	this.status = this.statuses.manual
+	var opcode = this.program.charCodeAt(this.ip) & 15
+	var operands = this.operandStack.length
+	var calls = this.callStack.length
+	var address, value, length, value2, mask
+
+	switch (opcode) {
+		case this.opcodes.hlt:
+			this.status = this.statuses.halt
+		break
+
+		case this.opcodes.ret:
+			if (calls) this.ip = this.callStack.pop()
+			else this.status = this.statuses.callStackEmpty
+		break
+
+		case this.opcodes.call:
+			if (calls >= 255) {
+				this.status = this.statuses.callStackFull
+				break
+			}
+		case this.opcodes.jmp:
+			if (!operands) this.status = this.statuses.operandStackEmpty
+			else this.ip = this.operandStack.pop()
+		break
+
+
+		case this.opcodes.ld:
+			if (!operands) this.status = this.statuses.operandStackEmpty
+			else this.operandStack.push(this.getMemory(this.operandStack.pop()))
+		break
+
+		case this.opcodes.st:
+			if (operands < 2) this.status = this.statuses.operandStackEmpty
+			else {
+				address = this.operandStack.pop()
+				value = this.operandStack.pop()
+				this.setMemory(address, value)
+			}
+		break
+
+		case this.opcodes.ins:
+			if (operands >= 255) this.status = this.statuses.operandStackFull
+			else {
+				value = this.input.read()
+				if (value == undefined) this.status = this.statuses.input
+				else this.operandStack.push(Number(value) || 0)
+			}
+		break
+
+		case this.opcodes.outs:
+			if (!operands) this.status = this.statuses.operandStackEmpty
+			else {
+				value = this.operandStack.pop()
+				if (!this.output.write(value)) {
+					this.operandStack.push(value)
+					this.status = this.statuses.output
+				}
+			}
+		break
+
+		case this.opcodes.sh:
+		case this.opcodes.sa:
+			if (operands < 2) {
+				this.status = this.statuses.operandStackEmpty
+				break
+			}
+
+			length = this.operandStack.pop()
+			if (!length) break
+
+			value = this.operandStack.pop()
+			var right = (length < 0)
+			if (right) length = -length
+			if (length >= 16) value = 0
+			else {
+				mask = 0
+				if (opcode == this.opcodes.sa) {
+					if (right) {
+						if (value & 0x8000) mask = 0xffff << (16 - length)
+					} else {
+						if (value & 1) mask = 0xffff >> (16 - length)
+					}
+				}
+				value = ((right ? value >> length : value << length) | mask) & 0xffff
+			}
+			this.operandStack.push(value)
+		break
+
+		case this.opcodes.test:
+		case this.opcodes.bt:
+			if (operands < 3) this.status = this.statuses.operandStackEmpty
+			else {
+				mask = this.operandStack.pop()
+				value2 = this.operandStack.pop()
+				value = this.operandStack.pop()
+				if (operand == this.operands.test) value = (mask ? value2 : value)
+				else value = (value2 & mask) | (value & (~mask))
+				this.operandStack.push(value)
+			}
+		break
+
+		case this.opcodes.dn:
+		case this.opcodes.db:
+		case this.opcodes.dw:
+			if (operands >= 255) {
+				this.status = this.statuses.operandStackFull
+				break
+			}
+
+			length = 1 << (opcode & 3)
+			value = 0
+			for (var i = 0; i < length; i++) {
+				this.ip = (this.ip + 1) & 0xffff
+				value = (value << 4) | (this.program.charCodeAt(this.ip) & 15)
+			}
+			length <<= 2
+			mask = 1 << (length - 1)
+			if (value & mask) value |= (~mask + 1 - mask)
+			this.operandStack.push(value)
+		break
+
+		case this.opcodes.dup:
+			if (opcodes >= 255) this.status = this.statuses.operandStackFull
+			else this.operandStack.push(opcodes ? this.operandStack[opcodes - 1] : 0)
+		break
+	}
+
+	if (this.status < this.statuses.breakPoint) {
+		this.ip = (this.ip + 1) & 0xffff
+		if (this.status == this.statuses.manual && this.ip in this.breakPointIndex) {
+			this.status = this.statuses.breakPoint
+		}
+	}
+	return this.status
+}
